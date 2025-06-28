675bae657b77014102b7baea22c101a38ab20510
mahendra varma <bsmv.08@gmail.com>
2025-06-28 22:48:48 +0530

    Added routes

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const profileRoute = express.Router()
-const {userAuth} = require('../middlewares/auth')
-
+const { userAuth } = require('../middlewares/auth')
+const { validateProfileUpdateFields } = require('../utils/validators')
 profileRoute.get("/profile/view", userAuth, async (req, res) => {
     try {
         const user = req.user
         res.send(user)
+    }
+    catch (err) {
+        res.status(400).send("Error: " + err.message)
+    }
+})
+
+
+profileRoute.get("/profile/edit", userAuth, async (req, res) => {
+    try {
+        if (!validateProfileUpdateFields(req)){
+           throw new Error("Cannot update mandatory fields.")
+        }
+        else{
+            const loggedInUser = req.user
+            
+        }
     }
     catch (err) {
         res.status(400).send("Error: " + err.message)
